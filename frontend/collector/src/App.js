60676ba5cde548c58459382f438f6bed385a2af6
Avishnavi <231001024@rajalakshmi.edu.in>
2026-05-14 10:31:44 +0530

    Comment out index.css import in App.js
    Comment out the import statement for index.css.

--- frontend/collector/src/App.js
+++ frontend/collector/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import './index.css';
+// import './index.css';
 import CollectorDashboard from './CollectorDashboard';
 import { authAPI } from './api';
 
